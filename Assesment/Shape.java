c5a8c48b2d58d11a4681ac148454328116d9502a
Samir Tahrian <samirtahrian1002@gmail.com>
2026-06-15 11:15:00 +0600

    Add files via upload

---
+++ Assesment/Shape.java
@@ -0,0 +1,17 @@
+package com.mycompany.assesment;
+
+abstract class Shape {
+    protected String color;
+    protected boolean filled;
+
+    public Shape(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public abstract double getArea();
+    public abstract double getPerimeter();
+
+    @Override
+    public abstract String toString();
+}
